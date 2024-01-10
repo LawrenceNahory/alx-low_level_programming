265197fa49c60ed10237c6ab643f6934c589fc2b
LawrenceNahory <nahory19@gmail.com>
2024-01-10 08:36:22 -0800

    low

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,22 @@
-#include <stdio.h>
+/*
+ * File: 0-puts_recursion.c
+ * Auth: Brennan D Baraban
+ */
 
-void print_string(char *s) {
-    printf("%s\n", s);
+#include "holberton.h"
+
+/**
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: The string to be printed.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
 }
-
